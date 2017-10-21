f5c7a5113d9bcc25549423905bdec0f322224f72
Eviatar Bach <eviatarbach@protonmail.com>
2017-10-21 19:04:51 -0400

    Adding multiple averaging times, model order selection

--- granger.m
+++ granger.m
@@ -1,6 +1,4 @@
 nobs = {13870, 2774, 924};
-morder = 10;
-alpha = 0.1;
 mhtc = 'FDR';
 
 cd mvgc_v1.0
@@ -19,12 +17,13 @@ vort_15 = importdata('data/vort_15.mat');
 sst = {sst_01, sst_05, sst_15};
 vort = {vort_01, vort_05, vort_15};
 
-F_vort_to_sst = {zeros(480, 241), zeros(480, 241), zeros(480, 241)};
-F_sst_to_vort = {zeros(480, 241), zeros(480, 241), zeros(480, 241)};
-sig_90_vort_to_sst = {zeros(480, 241), zeros(480, 241), zeros(480, 241)};
-sig_90_sst_to_vort = {zeros(480, 241), zeros(480, 241), zeros(480, 241)};
-sig_95_vort_to_sst = {zeros(480, 241), zeros(480, 241), zeros(480, 241)};
-sig_95_sst_to_vort = {zeros(480, 241), zeros(480, 241), zeros(480, 241)};
+F_vort_to_sst = {NaN(480, 241), NaN(480, 241), NaN(480, 241)};
+F_sst_to_vort = {NaN(480, 241), NaN(480, 241), NaN(480, 241)};
+sig_90_vort_to_sst = {NaN(480, 241), NaN(480, 241), NaN(480, 241)};
+sig_90_sst_to_vort = {NaN(480, 241), NaN(480, 241), NaN(480, 241)};
+sig_95_vort_to_sst = {NaN(480, 241), NaN(480, 241), NaN(480, 241)};
+sig_95_sst_to_vort = {NaN(480, 241), NaN(480, 241), NaN(480, 241)};
+times = {NaN(480, 241), NaN(480, 241), NaN(480, 241)};
 
 parpool(20)
 
@@ -34,28 +33,53 @@ parfor lat = 1:241
             lat, lon
             sst = reshape(sst{i}(lon, lat, :), [1, nobs{i}]);
             vort = reshape(vort{i}(lon, lat, :), [1, nobs{i}]);
-            if sst(1) == -9999
-                ratios(lon, lat) = NaN;
+
+            if (sst(1) == -9999) | (vort(1) == -9999)
                 continue
             end
 
             sst = detrend(sst);  % remove global warming signal
 
             X = [sst; vort];
-            [A, SIG] = tsdata_to_var(X, morder, 'OLS');
-            assert(~isbad(A),'VAR estimation failed');
-            [G,info] = var_to_autocov(A,SIG);
-            var_info(info,true);
+
+            [~, ~, moAIC, ~] = tsdata_to_infocrit(X, 50);
+	    times{i}(lon, lat) = moAIC;
+
+            [A, SIG] = tsdata_to_var(X, moAIC);
+            assert(~isbad(A), 'VAR estimation failed');
+            [G, info] = var_to_autocov(A, SIG);
+            var_info(info, true);
 
             F = autocov_to_pwcgc(G);
-            pval = mvgc_pval(F,morder,nobs{i},1,1,1,0);
-            sig = significance(pval,alpha,mhtc);
 
             F_1 = F(1, 2);  % vort -> sst
             F_2 = F(2, 1);  % sst -> vort
 
-            sig_1 = sig(1, 2);
-            sig_2 = sig(2, 1);
+	    F_vort_to_sst{i}(lon, lat) = F_1;
+	    F_sst_to_vort{i}(lon, lat) = F_2;
+
+            pval = mvgc_pval(F, moAIC, nobs{i}, 1, 1, 1, 0);
+
+	    for alpha = [0.1, 0.05]
+                sig = significance(pval, alpha, mhtc);
+		sig_1 = sig(1, 2);
+                sig_2 = sig(2, 1);
+		if alpha == 0.1
+                    sig_90_vort_to_sst{i}(lon, lat) = sig_1;
+                    sig_90_sst_to_vort{i}(lon, lat) = sig_2;
+                else
+                    sig_95_vort_to_sst{i}(lon, lat) = sig_1;
+                    sig_95_sst_to_vort{i}(lon, lat) = sig_2;
+		end
+            end
         end
     end
 end
+
+save('data/F_vort_to_sst.mat', 'F_vort_to_sst');
+save('data/F_sst_to_vort.mat', 'F_sst_to_vort');
+save('data/sig_90_vort_to_sst.mat', 'sig_90_vort_to_sst');
+save('data/sig_90_sst_to_vort.mat', 'sig_90_sst_to_vort');
+save('data/sig_95_vort_to_sst.mat', 'sig_95_vort_to_sst');
+save('data/sig_95_sst_to_vort.mat', 'sig_95_sst_to_vort');
+save('data/times.mat', 'times');
