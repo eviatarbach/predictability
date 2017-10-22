e4e54420e170271d3fd2eddaaae1155bd79a3012
Eviatar Bach <ebach@login-1.deepthought2.umd.edu>
2017-10-21 23:03:02 -0400

    fixing to work with parfor

--- granger.m
+++ granger.m
@@ -27,23 +27,35 @@ times = {NaN(480, 241), NaN(480, 241), NaN(480, 241)};
 
 parpool(20)
 
-parfor lat = 1:241
-    for lon = 1:480
-        for i = 1:3
-            lat, lon
-            sst = reshape(sst{i}(lon, lat, :), [1, nobs{i}]);
-            vort = reshape(vort{i}(lon, lat, :), [1, nobs{i}]);
+for i = 1:3
+    nobs_i = nobs{i};
+    sst_i = sst{i};
+    vort_i = vort{i};
 
-            if (sst(1) == -9999) | (vort(1) == -9999)
+    times_i = times{i}; % Need to do this for parfor to work
+    F_vort_to_sst_i = F_vort_to_sst{i};
+    F_sst_to_vort_i = F_sst_to_vort{i};
+    sig_90_sst_to_vort_i = sig_90_sst_to_vort{i};
+    sig_90_vort_to_sst_i = sig_90_vort_to_sst{i};
+    sig_95_sst_to_vort_i = sig_95_sst_to_vort{i};
+    sig_95_vort_to_sst_i = sig_95_vort_to_sst{i};
+
+    parfor lon = 1:480
+        for lat = 1:241
+            lat, lon
+            sst_ts = reshape(sst_i(lon, lat, :), [1, nobs_i]);
+            vort_ts = reshape(vort_i(lon, lat, :), [1, nobs_i]);
+
+            if (sst_ts(1) == -9999) | (vort_ts(1) == -9999)
                 continue
             end
 
-            sst = detrend(sst);  % remove global warming signal
+            sst_ts = detrend(sst_ts);  % remove global warming signal
 
-            X = [sst; vort];
+            X = [sst_ts; vort_ts];
 
             [~, ~, moAIC, ~] = tsdata_to_infocrit(X, 50);
-	    times{i}(lon, lat) = moAIC;
+            times_i(lon, lat) = moAIC;
 
             [A, SIG] = tsdata_to_var(X, moAIC);
             assert(~isbad(A), 'VAR estimation failed');
@@ -55,25 +67,32 @@ parfor lat = 1:241
             F_1 = F(1, 2);  % vort -> sst
             F_2 = F(2, 1);  % sst -> vort
 
-	    F_vort_to_sst{i}(lon, lat) = F_1;
-	    F_sst_to_vort{i}(lon, lat) = F_2;
+            F_vort_to_sst_i(lon, lat) = F_1;
+            F_sst_to_vort_i(lon, lat) = F_2;
 
-            pval = mvgc_pval(F, moAIC, nobs{i}, 1, 1, 1, 0);
+            pval = mvgc_pval(F, moAIC, nobs_i, 1, 1, 1, 0);
 
-	    for alpha = [0.1, 0.05]
+            for alpha = [0.1, 0.05]
                 sig = significance(pval, alpha, mhtc);
-		sig_1 = sig(1, 2);
+                sig_1 = sig(1, 2);
                 sig_2 = sig(2, 1);
-		if alpha == 0.1
-                    sig_90_vort_to_sst{i}(lon, lat) = sig_1;
-                    sig_90_sst_to_vort{i}(lon, lat) = sig_2;
+                if alpha == 0.1
+                    sig_90_vort_to_sst_i(lon, lat) = sig_1;
+                    sig_90_sst_to_vort_i(lon, lat) = sig_2;
                 else
-                    sig_95_vort_to_sst{i}(lon, lat) = sig_1;
-                    sig_95_sst_to_vort{i}(lon, lat) = sig_2;
-		end
+                    sig_95_vort_to_sst_i(lon, lat) = sig_1;
+                    sig_95_sst_to_vort_i(lon, lat) = sig_2;
+                end
             end
         end
     end
+    times{i} = times_i;
+    F_vort_to_sst{i} = F_vort_to_sst_i;
+    F_sst_to_vort{i} = F_sst_to_vort_i;
+    sig_90_sst_to_vort{i} = sig_90_sst_to_vort_i;
+    sig_90_vort_to_sst{i} = sig_90_vort_to_sst_i;
+    sig_95_sst_to_vort{i} = sig_95_sst_to_vort_i;
+    sig_95_vort_to_sst{i} = sig_95_vort_to_sst_i;
 end
 
 save('data/F_vort_to_sst.mat', 'F_vort_to_sst');
