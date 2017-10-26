50c451b88b76aed0432ebe8fead5629c8ea50ea1
Eviatar Bach <eviatarbach@protonmail.com>
2017-10-26 11:27:07 -0400

    fixing delayed code

--- granger_delayed.m
+++ granger_delayed.m
@@ -18,19 +18,21 @@ vort_15 = importdata('data/vort_15.mat');
 sst = {sst_01, sst_05, sst_15};
 vort = {vort_01, vort_05, vort_15};
 
-F_vort_to_sst = {NaN(480, 241), {NaN(480, 241), NaN(480, 241), NaN(480, 241)},...
-                 NaN(480, 241)};
-F_sst_to_vort = {NaN(480, 241), {NaN(480, 241), NaN(480, 241), NaN(480, 241)},...
-                 NaN(480, 241)};
-sig_90_vort_to_sst = {NaN(480, 241), {NaN(480, 241), NaN(480, 241), NaN(480, 241)},...
-                 NaN(480, 241)};
-sig_90_sst_to_vort = {NaN(480, 241), {NaN(480, 241), NaN(480, 241), NaN(480, 241)},...
-                 NaN(480, 241)};
-sig_95_vort_to_sst = {NaN(480, 241), {NaN(480, 241), NaN(480, 241), NaN(480, 241)},...
-                 NaN(480, 241)};
-sig_95_sst_to_vort = {NaN(480, 241), {NaN(480, 241), NaN(480, 241), NaN(480, 241)},...
-                 NaN(480, 241)};
+F_vort_to_sst = {{NaN(480, 241)}, {NaN(480, 241), NaN(480, 241), NaN(480, 241)},...
+                 {NaN(480, 241)}};
+F_sst_to_vort = {{NaN(480, 241)}, {NaN(480, 241), NaN(480, 241), NaN(480, 241)},...
+                 {NaN(480, 241)}};
+sig_90_vort_to_sst = {{NaN(480, 241)}, {NaN(480, 241), NaN(480, 241), NaN(480, 241)},...
+                 {NaN(480, 241)}};
+sig_90_sst_to_vort = {{NaN(480, 241)}, {NaN(480, 241), NaN(480, 241), NaN(480, 241)},...
+                 {NaN(480, 241)}};
+sig_95_vort_to_sst = {{NaN(480, 241)}, {NaN(480, 241), NaN(480, 241), NaN(480, 241)},...
+                 {NaN(480, 241)}};
+sig_95_sst_to_vort = {{NaN(480, 241)}, {NaN(480, 241), NaN(480, 241), NaN(480, 241)},...
+                 {NaN(480, 241)}};
 times = importdata('data/times.mat');
+
+parpool(20)
 
 for i = 1:3
     nobs_i = nobs{i};
@@ -46,26 +48,34 @@ for i = 1:3
     sig_95_sst_to_vort_i = sig_95_sst_to_vort{i};
     sig_95_vort_to_sst_i = sig_95_vort_to_sst{i};
 
-    for lon = 1:480
-        for lat = 1:241
-            lat, lon
-            sst_ts = reshape(sst_i(lon, lat, :), [1, nobs_i]);
-            vort_ts = reshape(vort_i(lon, lat, :), [1, nobs_i]);
+    j = 1;
 
-            if (sst_ts(1) == -9999) | (vort_ts(1) == -9999)
-                continue
-            end
+    for delay = delays_i
+        F_vort_to_sst_ij = F_vort_to_sst_i{j};
+        F_sst_to_vort_ij = F_sst_to_vort_i{j};
+        sig_90_vort_to_sst_ij = sig_90_vort_to_sst_i{j};
+        sig_90_sst_to_vort_ij = sig_90_sst_to_vort_i{j};
+        sig_95_vort_to_sst_ij = sig_95_vort_to_sst_i{j};
+        sig_95_sst_to_vort_ij = sig_95_sst_to_vort_i{j};
 
-            sst_ts = detrend(sst_ts);  % remove global warming signal
+        parfor lon = 1:480
+            for lat = 1:241
+                lat, lon
+                sst_ts = reshape(sst_i(lon, lat, :), [1, nobs_i]);
+                vort_ts = reshape(vort_i(lon, lat, :), [1, nobs_i]);
 
-            j = 1;
-            for delay = delays_i
+                if (sst_ts(1) == -9999) || (vort_ts(1) == -9999)
+                    continue
+                end
+
+                sst_ts = detrend(sst_ts);  % remove global warming signal
+
                 moAIC = times_i(lon, lat);
 
-                sst_ts_effect = sst_ts(delay:end);
+                sst_ts_effect = sst_ts(1+delay:end);
                 sst_ts_cause = sst_ts(1:end-delay);
 
-                vort_ts_effect = vort_ts(delay:end);
+                vort_ts_effect = vort_ts(1+delay:end);
                 vort_ts_cause = vort_ts(1:end-delay);
 
                 X_sst_cause = [sst_ts_cause; vort_ts_effect];
@@ -89,8 +99,8 @@ for i = 1:3
                 F_1 = autocov_to_mvgc(G_vort_cause, [1], [2]);  % vort -> sst
                 F_2 = autocov_to_mvgc(G_sst_cause, [2], [1]);  % sst -> vort
 
-                F_vort_to_sst_i{j}(lon, lat) = F_1;
-                F_sst_to_vort_i{j}(lon, lat) = F_2;
+                F_vort_to_sst_ij(lon, lat) = F_1;
+                F_sst_to_vort_ij(lon, lat) = F_2;
 
                 pval_vort_cause = mvgc_pval(F_1, moAIC, nobs_i - delay, 1, 1, 1, 0);
                 pval_sst_cause = mvgc_pval(F_2, moAIC, nobs_i - delay, 1, 1, 1, 0);
@@ -99,16 +109,23 @@ for i = 1:3
                     sig_vort_cause = significance(pval_vort_cause, alpha, mhtc);
                     sig_sst_cause = significance(pval_sst_cause, alpha, mhtc);
                     if alpha == 0.1
-                        sig_90_vort_to_sst_i{j}(lon, lat) = sig_vort_cause;
-                        sig_90_sst_to_vort_i{j}(lon, lat) = sig_sst_cause;
+                        sig_90_vort_to_sst_ij(lon, lat) = sig_vort_cause;
+                        sig_90_sst_to_vort_ij(lon, lat) = sig_sst_cause;
                     else
-                        sig_95_vort_to_sst_i{j}(lon, lat) = sig_vort_cause;
-                        sig_95_sst_to_vort_i{j}(lon, lat) = sig_sst_cause;
+                        sig_95_vort_to_sst_ij(lon, lat) = sig_vort_cause;
+                        sig_95_sst_to_vort_ij(lon, lat) = sig_sst_cause;
                     end
                 end
-                j = j + 1;
             end
         end
+        F_vort_to_sst_i{j} = F_vort_to_sst_ij;
+        F_sst_to_vort_i{j} = F_sst_to_vort_ij;
+        sig_90_vort_to_sst_i{j} = sig_90_vort_to_sst_ij;
+        sig_90_sst_to_vort_i{j} = sig_90_sst_to_vort_ij;
+        sig_95_vort_to_sst_i{j} = sig_95_vort_to_sst_ij;
+        sig_95_sst_to_vort_i{j} = sig_95_sst_to_vort_ij;
+
+        j = j + 1;
     end
     F_vort_to_sst{i} = F_vort_to_sst_i;
     F_sst_to_vort{i} = F_sst_to_vort_i;
