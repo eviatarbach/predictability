839f603cc3bdb293fd5021098944781ee251bfe4
Eviatar Bach <eviatarbach@protonmail.com>
2017-11-11 02:32:38 -0500

    adding physical method plots

--- plot_comparison.py
+++ plot_comparison.py
@@ -3,6 +3,7 @@ import cartopy.feature as cfeature
 from cartopy.util import add_cyclic_point
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
+from matplotlib import ticker
 import cmocean
 import scipy.io
 import numpy
@@ -11,7 +12,7 @@ F_sst_to_vort = scipy.io.loadmat('data/F_sst_to_vort.mat')['F_sst_to_vort'][0][1
 F_vort_to_sst = scipy.io.loadmat('data/F_vort_to_sst.mat')['F_vort_to_sst'][0][1]
 
 for var in ['ratio', 'atmos', 'ocean', 'both']:
-    plt.figure(figsize=(6, 3))
+    plt.figure()
     ax = plt.axes(projection=ccrs.PlateCarree())
     ax.add_feature(cfeature.LAND, color='#D9D9D9')
     ax.add_feature(cfeature.COASTLINE, linewidth=0.3)
@@ -21,18 +22,16 @@ for var in ['ratio', 'atmos', 'ocean', 'both']:
 
     if var == 'ratio':
         ratios = numpy.log(F_vort_to_sst/F_sst_to_vort)
-        ratios[ratios > 5] = 5
-        ratios[ratios < -5] = -5
         var_to_plot = ratios
         levels = numpy.linspace(-5, 5, 40)
         cmap = cmocean.cm.balance
     elif var == 'atmos':
         var_to_plot = F_vort_to_sst
-        levels = numpy.linspace(0, numpy.nanmax(F_vort_to_sst), 40)
+        levels = numpy.linspace(0, 0.15, 40)
         cmap = cmocean.cm.amp
     elif var == 'ocean':
         var_to_plot = F_sst_to_vort
-        levels = numpy.linspace(0, numpy.nanmax(F_sst_to_vort), 40)
+        levels = numpy.linspace(0, 0.04, 40)
         upper_half = numpy.linspace(0.5, 1, 40)
         cmap = cmocean.cm.balance_r
         colors = cmap(upper_half)
@@ -49,10 +48,13 @@ for var in ['ratio', 'atmos', 'ocean', 'both']:
     latt, lonn = numpy.meshgrid(lat, lon_cyc)
 
     plt.contourf(lonn, latt, var_cyc.T, vmin=min(levels), vmax=max(levels),
-                 cmap=cmap,
-                 levels=levels,
-                 transform=ccrs.PlateCarree())
-    plt.colorbar()
+                 cmap=cmap, levels=levels, transform=ccrs.PlateCarree(),
+                 extend='both')
+    cb = plt.colorbar(orientation='horizontal', fraction=0.05, pad=0.04)
+
+    tick_locator = ticker.MaxNLocator(nbins=9)
+    cb.locator = tick_locator
+    cb.update_ticks()
 
     plt.savefig('map_comparison_{var}.eps'.format(var=var))
 
---
+++ plot_physical_method.py
@@ -0,0 +1,57 @@
+import numpy
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.util import add_cyclic_point
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib import ticker
+import cmocean
+
+daily = numpy.fromfile('data/freq_sstvort850ah_eraint_0.75x0.75_daily365_1979-2016.dat', numpy.float32)
+daily[daily == -9999] = numpy.nan
+daily = daily.reshape([3, 4, 241, 480])
+
+pentad = numpy.fromfile('data/freq_sstvort850ah_eraint_0.75x0.75_pentad_1979-2016.dat', numpy.float32)
+pentad[pentad == -9999] = numpy.nan
+pentad = pentad.reshape([3, 4, 241, 480])
+
+for i, data in enumerate([daily, pentad]):
+    for j, var in enumerate(['ocean', 'atmos']):
+        anomalies = data[0, 2*j, :, :] + data[0, 2*j + 1, :, :]
+        plt.figure()
+        ax = plt.axes(projection=ccrs.PlateCarree())
+        ax.add_feature(cfeature.LAND, color='#D9D9D9')
+        ax.add_feature(cfeature.COASTLINE, linewidth=0.3)
+
+        lat = numpy.arange(-90, -90 + 241*0.75, 0.75)
+        lon = numpy.arange(0, 480*0.75, 0.75)
+
+        anomalies_cyc, lon_cyc = add_cyclic_point(anomalies, coord=lon)
+        
+        latt, lonn = numpy.meshgrid(lat, lon_cyc)
+
+        if var == 'atmos':
+            levels = numpy.linspace(numpy.nanmedian(anomalies),
+                                    numpy.nanmax(anomalies), 40)
+            cmap = cmocean.cm.amp
+        elif var == 'ocean':
+            levels = numpy.linspace(numpy.nanmedian(anomalies),
+                                    numpy.nanmax(anomalies), 40)
+            upper_half = numpy.linspace(0.5, 1, 40)
+            cmap = cmocean.cm.balance_r
+            colors = cmap(upper_half)
+            cmap = LinearSegmentedColormap.from_list('Upper Half', colors)
+
+        plt.contourf(lonn, latt, anomalies_cyc.T, vmin=min(levels),
+                     vmax=max(levels), cmap=cmap, levels=levels,
+                     transform=ccrs.PlateCarree(), extend='both')
+        cb = plt.colorbar(orientation='horizontal', fraction=0.05, pad=0.04)
+        cb.set_label('Anomaly count')
+        tick_locator = ticker.MaxNLocator(nbins=9)
+        cb.locator = tick_locator
+        cb.update_ticks()
+
+        plt.title('{var} forcing using dynamical rule ({resolution} resolution)'.format(var=['Oceanic', 'Atmospheric'][j], resolution=['daily', 'pentad'][i]))
+        plt.savefig('map_physical_{resolution}_{var}.eps'.format(resolution=['daily', 'pentad'][i], var=var))
+
+        plt.gcf().clear()
