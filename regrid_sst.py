02860a9fc16e7320c1bfd76d3bf1ed11819dc8eb
Eviatar Bach <eviatarbach@protonmail.com>
2018-08-15 19:33:51 -0400

    Fixing bug

--- regrid_sst.py
+++ regrid_sst.py
@@ -1,5 +1,6 @@
 import pygrib
 import numpy
+import hdf5storage
 
 dat = pygrib.open('/lustre/ebach/sfc.grib').select()
 
@@ -8,11 +9,17 @@ for i, dat2 in enumerate(dat):
     dat2.expand_grid(False)
     all_dat[i, :] = dat2.data()[0].data
 
-all_dat2 = all_dat.reshape([4, -1, 88838]).mean(axis=0)
-numpy.save('sst_g.npy', all_dat2)
+all_dat2 = all_dat.reshape([56980//4, -1, 88838]).mean(axis=1)
+matfiledata = {}
+matfiledata['sst'] = all_dat2
+hdf5storage.write(matfiledata, '.', 'data/sst01_g.mat', matlab_compatible=True)
 
-all_dat5 = all_dat2.reshape([5, -1, 88838]).mean(axis=0)
-numpy.save('sst5_g.npy', all_dat5)
+all_dat5 = all_dat2.reshape([14245//5, -1, 88838]).mean(axis=1)
+matfiledata = {}
+matfiledata['sst'] = all_dat5
+hdf5storage.write(matfiledata, '.', 'data/sst05_g.mat', matlab_compatible=True)
 
-all_dat15 = all_dat5[:2847, :].reshape([3, -1, 88838]).mean(axis=0)
-numpy.save('sst15_g.npy', all_dat15)
+all_dat15 = all_dat5[:2847, :].reshape([2847//3, -1, 88838]).mean(axis=1)
+matfiledata = {}
+matfiledata['sst'] = all_dat15
+hdf5storage.write(matfiledata, '.', 'data/sst15_g.mat', matlab_compatible=True)
--- regrid_vort.py
+++ regrid_vort.py
@@ -1,18 +1,25 @@
 import pygrib
 import numpy
+import hdf5storage
 
 dat = pygrib.open('/lustre/ebach/pl.grib').select()
 
 all_dat = numpy.zeros([56980, 88838])
-for i, dat2 in enumerate(dat[::2]):
+for i, dat2 in enumerate(dat):
     dat2.expand_grid(False)
     all_dat[i, :] = dat2.data()[0].data
 
-all_dat2 = all_dat.reshape([4, -1, 88838]).mean(axis=0)
-numpy.save('vort_g.npy', all_dat2)
+all_dat2 = all_dat.reshape([56980//4, -1, 88838]).mean(axis=1)
+matfiledata = {}
+matfiledata['vort'] = all_dat2
+hdf5storage.write(matfiledata, '.', 'data/vort01_g.mat', matlab_compatible=True)
 
-all_dat5 = all_dat2.reshape([5, -1, 88838]).mean(axis=0)
-numpy.save('vort5_g.npy', all_dat5)
+all_dat5 = all_dat2.reshape([14245//5, -1, 88838]).mean(axis=1)
+matfiledata = {}
+matfiledata['vort'] = all_dat5
+hdf5storage.write(matfiledata, '.', 'data/vort05_g.mat', matlab_compatible=True)
 
-all_dat15 = all_dat5[:2847, :].reshape([3, -1, 88838]).mean(axis=0)
-numpy.save('vort15_g.npy', all_dat15)
+all_dat15 = all_dat5[:2847, :].reshape([2847//3, -1, 88838]).mean(axis=1)
+matfiledata = {}
+matfiledata['vort'] = all_dat15
+hdf5storage.write(matfiledata, '.', 'data/vort15_g.mat', matlab_compatible=True)
