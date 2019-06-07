590a144880bfcef0f6a5a77d6a286a85998e6425
Eviatar Bach <eviatarbach@protonmail.com>
2019-06-07 19:50:47 -0400

    Merging anomaly regridding

--- regrid_anomalies.py
+++ regrid_anomalies.py
@@ -1,34 +1,41 @@
 import numpy
 import hdf5storage
 
-all_dat2 = numpy.fromfile('vortah850_daily365_1979-2017.dat', dtype='f8').reshape([14235, 88838])
+all_dat2 = numpy.fromfile('vortah850_daily365_1979-2017.dat',
+                          dtype='f8').reshape([14235, 88838])
 
 matfiledata = {}
 matfiledata['vort'] = all_dat2
-hdf5storage.write(matfiledata, '.', 'data/vort01_365.mat', matlab_compatible=True)
+hdf5storage.write(matfiledata, '.', 'data/vort01_365.mat',
+                  matlab_compatible=True)
 
 all_dat5 = all_dat2.reshape([14235//5, -1, 88838]).mean(axis=1)
 matfiledata = {}
 matfiledata['vort'] = all_dat5
-hdf5storage.write(matfiledata, '.', 'data/vort05_365.mat', matlab_compatible=True)
+hdf5storage.write(matfiledata, '.', 'data/vort05_365.mat',
+                  matlab_compatible=True)
 
-all_dat15 = all_dat5.reshape([2847//3, -1, 88838]).mean(axis=1)
-matfiledata = {}
-matfiledata['vort'] = all_dat15
-hdf5storage.write(matfiledata, '.', 'data/vort15_365.mat', matlab_compatible=True)
-
-all_dat2 = numpy.fromfile('sstah_daily365_1979-2017.dat', dtype='f8').reshape([14235, 88838])
+all_dat2 = numpy.fromfile('sstah_daily365_1979-2017.dat',
+                          dtype='f8').reshape([14235, 88838])
 
 matfiledata = {}
 matfiledata['sst'] = all_dat2
-hdf5storage.write(matfiledata, '.', 'data/sst01_365.mat', matlab_compatible=True)
+hdf5storage.write(matfiledata, '.', 'data/sst01_365.mat',
+                  matlab_compatible=True)
 
 all_dat5 = all_dat2.reshape([14235//5, -1, 88838]).mean(axis=1)
 matfiledata = {}
 matfiledata['sst'] = all_dat5
-hdf5storage.write(matfiledata, '.', 'data/sst05_365.mat', matlab_compatible=True)
+hdf5storage.write(matfiledata, '.', 'data/sst05_365.mat',
+                  matlab_compatible=True)
 
-all_dat15 = all_dat5.reshape([2847//3, -1, 88838]).mean(axis=1)
-matfiledata = {}
-matfiledata['sst'] = all_dat15
-hdf5storage.write(matfiledata, '.', 'data/sst15_365.mat', matlab_compatible=True)
+for var in ['temp', 'q', 'pressure', 'div']:
+    all_dat = numpy.fromfile('{var}ah_daily365_1979-2017.dat'.format(var=var),
+                             dtype='f8').reshape([14235, 88838])
+
+    matfiledata = {}
+    matfiledata[var] = all_dat
+    hdf5storage.write(matfiledata, '.', 'data/{var}01_365.mat'.format(var),
+                      matlab_compatible=True)
+
+    all_dat.close()
