ff14803dc35ab1825eebd39c1301d20e46f82625
Eviatar Bach <eviatarbach@protonmail.com>
2019-06-09 22:06:39 -0400

    Removing old file, pentad

--- regrid_anomalies.py
+++ regrid_anomalies.py
@@ -1,35 +1,7 @@
 import numpy
 import hdf5storage
 
-all_dat2 = numpy.fromfile('vortah850_daily365_1979-2017.dat',
-                          dtype='f8').reshape([14235, 88838])
-
-matfiledata = {}
-matfiledata['vort'] = all_dat2
-hdf5storage.write(matfiledata, '.', 'data/vort01_365.mat',
-                  matlab_compatible=True)
-
-all_dat5 = all_dat2.reshape([14235//5, -1, 88838]).mean(axis=1)
-matfiledata = {}
-matfiledata['vort'] = all_dat5
-hdf5storage.write(matfiledata, '.', 'data/vort05_365.mat',
-                  matlab_compatible=True)
-
-all_dat2 = numpy.fromfile('sstah_daily365_1979-2017.dat',
-                          dtype='f8').reshape([14235, 88838])
-
-matfiledata = {}
-matfiledata['sst'] = all_dat2
-hdf5storage.write(matfiledata, '.', 'data/sst01_365.mat',
-                  matlab_compatible=True)
-
-all_dat5 = all_dat2.reshape([14235//5, -1, 88838]).mean(axis=1)
-matfiledata = {}
-matfiledata['sst'] = all_dat5
-hdf5storage.write(matfiledata, '.', 'data/sst05_365.mat',
-                  matlab_compatible=True)
-
-for var in ['temp', 'q', 'sp', 'div']:
+or var in ['vort', 'sst', 'temp', 'q', 'sp', 'div']:
     all_dat = numpy.fromfile('{var}ah_daily365_1979-2017.dat'.format(var=var),
                              dtype='f8').reshape([14235, 88838])
 
