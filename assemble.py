4bd6708e86209280f03010a24dd9c4d1c15730f1
Eviatar Bach <eviatarbach@protonmail.com>
2019-10-04 00:37:15 -0400

    Update assemble.py

--- assemble.py
+++ assemble.py
@@ -14,12 +14,6 @@ vars1 = dict(zip(all_vars, [(('cell', 'delay'), numpy.full((88838, 361), numpy.N
 data1 = xarray.Dataset(vars1, coords={'cell': range(1, 88839),
                                       'delay': range(0, 361)})
 
-vars5 = dict(zip(all_vars, [(('cell', 'delay'), numpy.full((88838, 181), numpy.NaN)) for _ in range(len(all_vars))]))
-data5 = xarray.Dataset(vars5, coords={'cell': range(1, 88839), 'delay': range(0, 181)})
-
-vars15 = dict(zip(all_vars, [(('cell', 'delay'), numpy.full((88838, 121), numpy.NaN)) for _ in range(len(all_vars))]))
-data15 = xarray.Dataset(vars15, coords={'cell': range(1, 88839), 'delay': range(0, 121)})
-
 for offset in range(1, 88838, 60):
     end = min(88838 - offset + 1, 60)
     for var in variables:
@@ -29,8 +23,6 @@ for offset in range(1, 88838, 60):
                 mat = scipy.io.loadmat('data_atmos/{name}_{cell}.mat'.format(name=name,
                                                                        cell=offset))[name][0]
                 data1[name][offset - 1:offset + 60 - 1, :] = mat[0][:end, :]
-                data5[name][offset - 1:offset + 60 - 1, :] = mat[1][:end, :]
-                data15[name][offset - 1:offset + 60 - 1, :] = mat[2][:end, :]
             except Exception as a:
                 pass
                 #print(offset)
@@ -38,11 +30,7 @@ for offset in range(1, 88838, 60):
     try:
         mat = scipy.io.loadmat('data_atmos/times_atmos_{cell}.mat'.format(cell=offset))['times'][0]
         data1['times'][offset - 1:offset + 60 - 1, 0] = mat[0][:end, 0]
-        data5['times'][offset - 1:offset + 60 - 1, 0] = mat[1][:end, 0]
-        data15['times'][offset - 1:offset + 60 - 1, 0] = mat[2][:end, 0]
     except Exception as a:
         print(offset)
 
 data1.to_netcdf('data_atmos/data01.nc')
-data5.to_netcdf('data_atmos/data05.nc')
-data15.to_netcdf('data_atmos/data15.nc')
